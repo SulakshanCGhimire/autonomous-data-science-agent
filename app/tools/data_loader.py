5ffae4aa03e9764f5b6faf28e1311cc54275eadd
Sulakshan Chandra Ghimire <sulucghimire@gmail.com>
2026-06-14 07:54:05 +0545

    Upgrading our simple load_csv() function into a formal object-oriented class called DatasetLoader

--- app/tools/data_loader.py
+++ app/tools/data_loader.py
@@ -1,5 +1,14 @@
 import pandas as pd
 
-def load_csv(path):
-    """Loads a CSV file into a pandas DataFrame."""
-    return pd.read_csv(path)
+class DatasetLoader:
+    def __init__(self):
+        """Initialize the loader with empty state."""
+        self.filepath = None
+        self.data = None
+
+    def load(self, path):
+        """Load a CSV file into the data attribute."""
+        self.filepath = path
+        # We will add our robust validation steps here shortly
+        self.data = pd.read_csv(self.filepath)
+        return self.data
