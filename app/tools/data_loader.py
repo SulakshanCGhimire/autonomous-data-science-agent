09355b0dcd4bc263c1f93f3b5bd44b9c7feed759
Sulakshan Chandra Ghimire <sulucghimire@gmail.com>
2026-06-14 08:25:21 +0545

    Add file validation method to DatasetLoader class

--- app/tools/data_loader.py
+++ app/tools/data_loader.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 class DatasetLoader:
@@ -5,10 +6,17 @@ class DatasetLoader:
         """Initialize the loader with empty state."""
         self.filepath = None
         self.data = None
+        
+    def validate_file(self, path):
+        """Validate the file path and format."""
+        if not os.path.isfile(path):
+            raise FileNotFoundError(f"The file {path} does not exist.")
+        return True
 
     def load(self, path):
         """Load a CSV file into the data attribute."""
         self.filepath = path
+        self.validate_file(self.filepath)
         # We will add our robust validation steps here shortly
         self.data = pd.read_csv(self.filepath)
         return self.data
